1db93b3a2a868d71557912a4322364488d56c351
JohnLester <jonhlesterbalucas.pamplona@gmail.com>
2021-06-25 23:14:50 +0700

    Done adding functionality to Calculate Wealth Button

--- js/index.js
+++ js/index.js
@@ -30,6 +30,22 @@ async function getRandomUser() {
     }
 }
 
+// Calculate the total wealth 
+function calculateWealth() {
+    const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+
+    const wealthEl = document.createElement('div');
+    wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(wealth)}</strong></h3>`;
+    main.appendChild(wealthEl);
+}
+
+// Show Millionaires
+function showMillionaires() {
+    data = data.filter(cur => cur.money > 1000000);
+
+    updateDOM();
+}
+
 // Sort users by richest
 function sortByRichest() {
     data.sort((a, b) => b.money - a.money);
@@ -76,3 +92,5 @@ function formatMoney(number) {
 addUserBtn.addEventListener('click', getRandomUser);
 doubleBtn.addEventListener('click', doubleMoney);
 sortBtn.addEventListener('click', sortByRichest);
+showMillionairesBtn.addEventListener('click', showMillionaires);
+calculateWealthBtn.addEventListener('click', calculateWealth);
